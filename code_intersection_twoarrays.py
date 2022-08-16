10b9e1450617375bcfef47b4a72fa06b2332d65d
Mehran Kader <kmehran.1106@gmail.com>
2022-08-17 01:08:01 +0600

    update: remove dependencies

--- code_intersection_twoarrays.py
+++ code_intersection_twoarrays.py
@@ -6,10 +6,25 @@ Problem Statement:
 
 import pytest
 from typing import List, NamedTuple
-from common_binary_search import Solution as BinarySearch
 
 
 class Solution:
+    def _binary_search(self, arr: List[int], value: int) -> int:
+        low, high = 0, len(arr) - 1
+
+        while low <= high:
+            _mid = (high + low) // 2
+            _value = arr[_mid]
+
+            if _value < value:
+                low = _mid + 1
+            elif _value > value:
+                high = _mid - 1
+            else:
+                return _mid
+
+        return -1
+
     def execute(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1.sort()
         nums2.sort()
@@ -17,7 +32,7 @@ class Solution:
         result = list()
 
         for v in nums1:
-            _index = BinarySearch().execute(nums2, v)
+            _index = self._binary_search(nums2, v)
             if _index != -1 and result.count(v) < nums2.count(v):
                 result.append(nums2[_index])
 
