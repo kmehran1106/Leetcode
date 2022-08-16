17565e46b8e5e9b1c732697576dee2913fd7aace
Mehran Kader <kmehran.1106@gmail.com>
2022-08-17 01:17:12 +0600

    add: contains duplicate

---
+++ code_contains_duplicate.py
@@ -0,0 +1,41 @@
+"""
+Problem Statement:
+    https://leetcode.com/problems/contains-duplicate/
+"""
+
+
+import pytest
+from typing import List, NamedTuple
+
+
+class Solution:
+    def execute(self, nums: List[int]) -> bool:
+        print(nums)
+        _set = set()
+
+        for num in nums:
+            if num in _set:
+                return True
+
+            _set.add(num)
+
+        return False
+
+
+@pytest.fixture
+def get_fixtures():
+    first_input = [1, 2, 3, 4]
+    first_output = False
+
+    second_input = [1, 2, 3, 1]
+    second_output = True
+
+    return [
+        (first_input, first_output),
+        (second_input, second_output),
+    ]
+
+
+def test_code(get_fixtures):
+    for data in get_fixtures:
+        assert Solution().execute(data[0]) == data[1]
